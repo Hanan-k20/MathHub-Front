061a020e1fc77b4266d6d5f864c7210a2bdd3de8
twinsmaram-hub <twinsmaram@gmail.com>
2026-01-25 16:17:25 +0300

    edit in voteservices

--- src/services/voteService.js
+++ src/services/voteService.js
@@ -1,27 +1,23 @@
 import axios from "axios"
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/votes`
+const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL
 
 const authConfig = () => ({
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 })
 
- const submitvote = async (solutionId, formData) => {
+ const submitvote = async (problemId, solutionId) => {
   const token = localStorage.getItem("token")
   if (!token) throw new Error("User is not authenticated")
 
-  const payload = {
-    solution: solutionId,
-    ...formData,
-  }
 
-  const res = await axios.post(`${BASE_URL}/${solutionId}`, payload, authConfig())
+  const res = await axios.post(`${BASE_URL}/problems/${problemId}/solutions/${solutionId}/votes`,{}, authConfig())
   return res.data
 }
 
-  const getvote = async (solutionId) => {
-  const res = await axios.get(`${BASE_URL}/${solutionId}`, authConfig())
-  return res.data.allvotes
+  const getvote =async (problemId, solutionId) => {
+  const res = await axios.get(`${BASE_URL}/problems/${problemId}/solutions/${solutionId}/votes`, authConfig())
+  return res.data
 }
 
 export {
