e83902851ab71619d7d84113ad1850c7ed20b313
Hanan-k20 <98twinshanan@gmail.com>
2026-01-26 10:49:28 +0300

    add term file

--- src/components/problem/problemList.jsx
+++ src/components/problem/problemList.jsx
@@ -1,39 +1,40 @@
 import { useNavigate } from 'react-router';
 import VoteButton from './VoteButton';
+
 function ProblemList({ problems }) {
     const navigate = useNavigate();
 
-    if(!problems){
-    return <h1>Loading....</h1>
-  }
-  return (
-    
-     <div>
-      <h1> List</h1>
-      {
-        !problems.length ?
-        <div>No problems Found</div>
-        :
-        <ul>
-        {
-          problems.map(
-            (oneProblem) => (
-              <li key={oneProblem.id}>
+    if (!problems) {
+        return <h1>Loading....</h1>
+    }
+    return (
 
-                <button onClick={() => navigate(`/oneProblems/${oneProblem.id}`)} >
-                    {oneProblem.title}
-                    <br/>
-                    {oneProblem.user?.username || 'Unknown'}||<VoteButton problemId={oneProblem.id} />
+        <div>
+            <h1> List</h1>
+            {
+                !problems.length ?
+                    <div>No problems Found</div>
+                    :
+                    <ul>
+                        {
+                            problems.map(
+                                (oneProblem) => (
+                                    <li key={oneProblem.id}>
 
-                </button>
-                
-              </li>
-            )
-          )
-        }
-        </ul>}
-    </div>
-  )
+                                        <button onClick={() => navigate(`/oneProblems/${oneProblem.id}`)} >
+                                            {oneProblem.title}
+                                            <br />
+                                            {oneProblem.user?.username || 'Unknown'}||<VoteButton problemId={oneProblem.id} />
+
+                                        </button>
+
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>}
+        </div>
+    )
 }
 
 export default ProblemList
