3df2d4d392efb91097553d75f325cacf7d0a5767
twinsmaram-hub <twinsmaram@gmail.com>
2026-01-26 19:43:12 +0300

    modify ın route

--- src/Components/FlashCard/CardList.jsx
+++ src/Components/FlashCard/CardList.jsx
@@ -13,11 +13,12 @@ function CardList({ cards }) {
   return (
     <div className="container">
       <h1 id="H1">Here's all the Cards</h1>
+    
 
-
-      <div className="cards-grid">
-        {cards.map((oneCard) => (
+      <div className="cards-grid"> 
+        {allCards.map((oneCard) => (
           <div key={oneCard._id} className="card-item">
+            
             <div className="card-header">
               <h3>{oneCard.title}</h3>
             </div>
@@ -26,10 +27,12 @@ function CardList({ cards }) {
               <p>
                 Owner: <strong>{oneCard.owner?.username || "Unknown"}</strong>
               </p>
+              
               <Link to={`/cards/${oneCard.id}`} className="btn-card-details">
                 See Details
               </Link>
             </div>
+            
           </div>
         ))}
       </div>
