7c0d2efeea9510ec3b28f71b0b9f26a23f423b17
Hanan-k20 <98twinshanan@gmail.com>
2026-01-26 10:36:39 +0300

    build ProblemList component and setup navigation to details

--- src/components/problem/problemList.jsx
+++ src/components/problem/problemList.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useNavigate } from 'react-router';
+import VoteButton from './VoteButton';
+function ProblemList({ problems }) {
+    const navigate = useNavigate();
 
-function ProblemList() {
+    if(!problems){
+    return <h1>Loading....</h1>
+  }
   return (
-    <div>
-      
+    
+     <div>
+      <h1> List</h1>
+      {
+        !problems.length ?
+        <div>No problems Found</div>
+        :
+        <ul>
+        {
+          problems.map(
+            (oneProblem) => (
+              <li key={oneProblem.id}>
+
+                <button onClick={() => navigate(`/oneProblems/${oneProblem.id}`)} >
+                    {oneProblem.title}
+                    <br/>
+                    {oneProblem.user?.username || 'Unknown'}||<VoteButton problemId={oneProblem.id} />
+
+                </button>
+                
+              </li>
+            )
+          )
+        }
+        </ul>}
     </div>
   )
 }
